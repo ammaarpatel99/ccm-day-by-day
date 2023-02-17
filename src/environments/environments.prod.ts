3d373708bc5ab7cf2a045f09f4e2f00eeaeea2b1
Ammaar Patel <ammaarpatel99@gmail.com>
2023-02-17 16:46:50 +0000

    setup firebase

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzuoIJEpyAaM5oZGrINVCTP23gs33f2PA",
+    authDomain: "ccm-day-by-day.firebaseapp.com",
+    projectId: "ccm-day-by-day",
+    storageBucket: "ccm-day-by-day.appspot.com",
+    messagingSenderId: "755187195719",
+    appId: "1:755187195719:web:f7a735ef5c167da3fff662"
+  }
+};
